4d6d5a1429f26bb23cf7b17a9b2e7a972d17a501
soda <32971088+soda69@users.noreply.github.com>
2023-03-04 10:03:47 -0600

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -5,7 +5,6 @@
 #include "Math.cpp"
 #include "Level.cpp"
 #include "X11Utils.cpp"
-#include "ConfigLoader.cpp"
 
 class NoRecoil
 {
@@ -43,7 +42,7 @@ public:
             return;
 
         // adjust pitch
-        const double norecoilPitchStrength = m_configLoader->getNorecoilPitchStrength();
+        const double norecoilPitchStrength = ((float)rand()/RAND_MAX)*(float)(0.10) + m_configLoader->getNorecoilPitchStrength();
         const double punchPitch = m_localPlayer->getPunchPitch();
         if (punchPitch != 0)
         {
@@ -54,7 +53,7 @@ public:
         }
 
         // adjust yaw
-        const double norecoilYawStrength = m_configLoader->getNorecoilYawStrength();
+        const double norecoilYawStrength = ((float)rand()/RAND_MAX)*(float)(0.10) + m_configLoader->getNorecoilYawStrength();
         const double punchYaw = m_localPlayer->getPunchYaw();
         if (punchYaw != 0)
         {
